10fe95ac0dd0d12127fc786e381aa17301ad9249
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2025-08-13 16:40:10 +0200

    Implement Midi2 note-on and packet utilities

--- Tests/MIDI2Tests/Midi2NoteOnTests.swift
+++ Tests/MIDI2Tests/Midi2NoteOnTests.swift
@@ -2,7 +2,25 @@ import XCTest
 @testable import MIDI2
 
 final class Midi2NoteOnTests: XCTestCase {
-    func testPlaceholder() {
-        // TODO: Test Midi2NoteOn
+    func testRoundTrip() {
+        let group = Uint4(0x1)!
+        let channel = Uint4(0x2)!
+        let note = Uint7(60)!
+        let msg = Midi2NoteOn(group: group, channel: channel, note: note, velocity: 0x1234, attributeType: .none, attributeData: 0x5678)
+        let packet = msg.ump()
+        guard let parsed = Midi2NoteOn(ump: packet) else {
+            return XCTFail("failed to parse packet")
+        }
+        XCTAssertEqual(parsed, msg)
+    }
+
+    func testInvalidStatusReturnsNil() {
+        let packet = UmpPacket64(word0: 0, word1: 0)
+        XCTAssertNil(Midi2NoteOn(ump: packet))
+    }
+
+    func testParsingThrowsForWrongMessageType() {
+        let packet = UmpPacket64(word0: UInt32(0x5 << 28), word1: 0)
+        XCTAssertThrowsError(try Midi2NoteOn(parsingUMP: packet))
     }
 }
--- Tests/MIDI2Tests/UmpPacket32Tests.swift
+++ Tests/MIDI2Tests/UmpPacket32Tests.swift
@@ -2,7 +2,21 @@ import XCTest
 @testable import MIDI2
 
 final class UmpPacket32Tests: XCTestCase {
-    func testPlaceholder() {
-        // TODO: Test UmpPacket32
+    func testMidi1NoteOnRoundtrip() {
+        let bytes: [UInt8] = [0x90, 0x40, 0x7F]
+        let group = Uint4(0x2)!
+        guard let packet = UmpPacket32(midi1Bytes: bytes, group: group) else {
+            return XCTFail("failed to construct packet")
+        }
+        XCTAssertEqual(packet.midi1Bytes(), bytes)
+    }
+
+    func testProgramChangeRoundtrip() {
+        let bytes: [UInt8] = [0xC3, 0x22]
+        let group = Uint4(0x1)!
+        guard let packet = UmpPacket32(midi1Bytes: bytes, group: group) else {
+            return XCTFail("failed to construct packet")
+        }
+        XCTAssertEqual(packet.midi1Bytes(), bytes)
     }
 }

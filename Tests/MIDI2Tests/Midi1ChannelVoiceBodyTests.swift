efb25d33f853d7147920c0f9b821fb18aef00063
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2025-08-14 06:30:07 +0200

    Add tests for status nibble and channel voice

--- Tests/MIDI2Tests/Midi1ChannelVoiceBodyTests.swift
+++ Tests/MIDI2Tests/Midi1ChannelVoiceBodyTests.swift
@@ -2,7 +2,31 @@ import XCTest
 @testable import MIDI2
 
 final class Midi1ChannelVoiceBodyTests: XCTestCase {
-    func testPlaceholder() {
-        // TODO: Test Midi1ChannelVoiceBody
+    func testRoundTrip() throws {
+        let g = Uint4(0)!
+        let msg = Midi1ChannelVoiceMessage.noteOn(group: g, channel: Uint4(0)!, note: Uint7(0x3C)!, velocity: Uint7(0x40)!)
+        let packet = msg.ump()
+        XCTAssertEqual(packet.word, 0x20903C40)
+        XCTAssertEqual(Midi1ChannelVoiceMessage(ump: packet), msg)
+        XCTAssertEqual(try Midi1ChannelVoiceMessage(parsingUMP: packet), msg)
+
+        let bytes = [UInt8](msg.midi1Bytes())
+        XCTAssertEqual(bytes, [0x90, 0x3C, 0x40])
+        XCTAssertEqual(Midi1ChannelVoiceMessage(midi1Bytes: bytes, group: g)?.midi1Bytes(), bytes)
+        XCTAssertEqual(try Midi1ChannelVoiceMessage(parsingMidi1Bytes: bytes, group: g).midi1Bytes(), bytes)
+    }
+
+    func testInvalidMessageType() {
+        let g = Uint4(0)!
+        let packet = UmpPacket32(mt: 0x0, group: g, status: 0, data1: 0, data2: 0)
+        XCTAssertNil(Midi1ChannelVoiceMessage(ump: packet))
+        XCTAssertThrowsError(try Midi1ChannelVoiceMessage(parsingUMP: packet))
+    }
+
+    func testRangeEnforcement() {
+        let g = Uint4(0)!
+        let bytes: [UInt8] = [0x90, 0xFF, 0x40]
+        XCTAssertNil(Midi1ChannelVoiceMessage(midi1Bytes: bytes, group: g))
+        XCTAssertThrowsError(try Midi1ChannelVoiceMessage(parsingMidi1Bytes: bytes, group: g))
     }
 }
--- Tests/MIDI2Tests/Midi1StatusNibbleTests.swift
+++ Tests/MIDI2Tests/Midi1StatusNibbleTests.swift
@@ -2,7 +2,21 @@ import XCTest
 @testable import MIDI2
 
 final class Midi1StatusNibbleTests: XCTestCase {
-    func testPlaceholder() {
-        // TODO: Test Midi1StatusNibble
+    func testRoundTrip() {
+        for raw in 0x8...0xE {
+            let status = Midi1StatusNibble(UInt8(raw))
+            XCTAssertNotNil(status)
+            XCTAssertEqual(status?.rawValue, UInt8(raw))
+        }
+    }
+
+    func testGoldenVectors() {
+        XCTAssertEqual(Midi1StatusNibble(0x8), .noteOff)
+        XCTAssertEqual(Midi1StatusNibble(0xE), .pitchBend)
+    }
+
+    func testInvalidValues() {
+        XCTAssertNil(Midi1StatusNibble(0x7))
+        XCTAssertNil(Midi1StatusNibble(0xF))
     }
 }

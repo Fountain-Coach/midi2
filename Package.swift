8b5f0b1c9817433552642c81e855146910798f78
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2025-08-14 07:18:23 +0200

    Add midi2demo CLI with Note On command

--- Package.swift
+++ Package.swift
@@ -6,14 +6,21 @@ let package = Package(
     platforms: [.macOS(.v13), .iOS(.v16)],
     products: [
         .library(name: "MIDI2", targets: ["MIDI2"]),
-        .library(name: "MIDI2CI", targets: ["MIDI2CI"])
+        .library(name: "MIDI2CI", targets: ["MIDI2CI"]),
+        .executable(name: "midi2demo", targets: ["midi2demo"])
     ],
     dependencies: [
-        .package(url: "https://github.com/typelift/SwiftCheck.git", from: "0.12.0")
+        .package(url: "https://github.com/typelift/SwiftCheck.git", from: "0.12.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0")
     ],
     targets: [
         .target(name: "MIDI2"),
         .target(name: "MIDI2CI", dependencies: ["MIDI2"]),
+        .executableTarget(name: "midi2demo", dependencies: [
+            "MIDI2",
+            "MIDI2CI",
+            .product(name: "ArgumentParser", package: "swift-argument-parser")
+        ]),
         .testTarget(name: "MIDI2Tests", dependencies: ["MIDI2", "MIDI2CI"]),
         .testTarget(name: "Fuzz", dependencies: ["MIDI2", "SwiftCheck"], path: "Tests/Fuzz")
     ]

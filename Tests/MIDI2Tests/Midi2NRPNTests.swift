693b8859a0f6a4b6f117c8f36ac93b2533610dff
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2025-08-14 06:04:31 +0200

    Add MIDI 2.0 absolute NRPN and RPN messages with tests

--- Tests/MIDI2Tests/Midi2NRPNTests.swift
+++ Tests/MIDI2Tests/Midi2NRPNTests.swift
@@ -2,7 +2,41 @@ import XCTest
 @testable import MIDI2
 
 final class Midi2NRPNTests: XCTestCase {
-    func testPlaceholder() {
-        // TODO: Test Midi2NRPN
+    func testRoundTrip() throws {
+        let group = Uint4(0x2)!
+        let channel = Uint4(0x7)!
+        let address = Midi2NRPNAddress(rawValue: 0x1234)!
+        let value: UInt32 = 0xDEADBEEF
+        let msg = Midi2NRPN(group: group, channel: channel, address: address, value: value)
+        let pkt = msg.ump()
+        let decoded = try Midi2NRPN(parsingUMP: pkt)
+        XCTAssertEqual(decoded, msg)
+    }
+
+    func testEdgeCases() throws {
+        let group = Uint4(0x0)!
+        let channel = Uint4(0xF)!
+        let minAddr = Midi2NRPNAddress(rawValue: 0x0000)!
+        let min = Midi2NRPN(group: group, channel: channel, address: minAddr, value: 0)
+        let decodedMin = try Midi2NRPN(parsingUMP: min.ump())
+        XCTAssertEqual(decodedMin, min)
+
+        let maxAddr = Midi2NRPNAddress(rawValue: 0x3FFF)!
+        let max = Midi2NRPN(group: group, channel: channel, address: maxAddr, value: 0xFFFF_FFFF)
+        let decodedMax = try Midi2NRPN(parsingUMP: max.ump())
+        XCTAssertEqual(decodedMax, max)
+    }
+
+    func testDecodeFailure() {
+        let group = Uint4(0x1)!
+        let channel = Uint4(0x2)!
+        let address = Midi2NRPNAddress(rawValue: 0x1234)!
+        let value: UInt32 = 0x01020304
+        let msg = Midi2NRPN(group: group, channel: channel, address: address, value: value)
+        let pkt = msg.ump()
+        let bad = UmpPacket64(word0: (pkt.word0 & ~0x00F00000) | UInt32(0x2 << 20), word1: pkt.word1)
+        XCTAssertThrowsError(try Midi2NRPN(parsingUMP: bad))
+        let badIndex = UmpPacket64(word0: pkt.word0 | 0x00000080, word1: pkt.word1)
+        XCTAssertThrowsError(try Midi2NRPN(parsingUMP: badIndex))
     }
 }
--- Tests/MIDI2Tests/Midi2RPNTests.swift
+++ Tests/MIDI2Tests/Midi2RPNTests.swift
@@ -2,7 +2,41 @@ import XCTest
 @testable import MIDI2
 
 final class Midi2RPNTests: XCTestCase {
-    func testPlaceholder() {
-        // TODO: Test Midi2RPN
+    func testRoundTrip() throws {
+        let group = Uint4(0x3)!
+        let channel = Uint4(0x4)!
+        let address = Midi2RPNAddress(rawValue: 0x2345)!
+        let value: UInt32 = 0xCAFEBABE
+        let msg = Midi2RPN(group: group, channel: channel, address: address, value: value)
+        let pkt = msg.ump()
+        let decoded = try Midi2RPN(parsingUMP: pkt)
+        XCTAssertEqual(decoded, msg)
+    }
+
+    func testEdgeCases() throws {
+        let group = Uint4(0x0)!
+        let channel = Uint4(0x0)!
+        let minAddr = Midi2RPNAddress(rawValue: 0x0000)!
+        let min = Midi2RPN(group: group, channel: channel, address: minAddr, value: 0)
+        let decodedMin = try Midi2RPN(parsingUMP: min.ump())
+        XCTAssertEqual(decodedMin, min)
+
+        let maxAddr = Midi2RPNAddress(rawValue: 0x3FFF)!
+        let max = Midi2RPN(group: group, channel: channel, address: maxAddr, value: 0xFFFF_FFFF)
+        let decodedMax = try Midi2RPN(parsingUMP: max.ump())
+        XCTAssertEqual(decodedMax, max)
+    }
+
+    func testDecodeFailure() {
+        let group = Uint4(0x1)!
+        let channel = Uint4(0x2)!
+        let address = Midi2RPNAddress(rawValue: 0x2345)!
+        let value: UInt32 = 0x0A0B0C0D
+        let msg = Midi2RPN(group: group, channel: channel, address: address, value: value)
+        let pkt = msg.ump()
+        let bad = UmpPacket64(word0: (pkt.word0 & ~0x00F00000) | UInt32(0x3 << 20), word1: pkt.word1)
+        XCTAssertThrowsError(try Midi2RPN(parsingUMP: bad))
+        let badBank = UmpPacket64(word0: pkt.word0 | 0x00008000, word1: pkt.word1)
+        XCTAssertThrowsError(try Midi2RPN(parsingUMP: badBank))
     }
 }
